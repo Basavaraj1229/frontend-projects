627787b84a575c6a2bc4ee8b30ceaa88b7aba256
Basavaraj1229 <rajukokane61@gmail.com>
2025-06-11 11:10:32 +0530

    icon-hover-efecct

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 
-setInterval(particles,300);
-
-function particles(){
-
-    const div = document.querySelector("div");
-    const particles = document.createElement("span");
-
-    let sizeOfParticles = Math.random();
-
-    particles.style.width = 50 + sizeOfParticles + "px";
-    particles.style.height = 50 + sizeOfParticles + "px";
-
-    particles.style.top = Math.random() * innerHeight + "px";
-    particles.style.left = Math.random() * innerWidth + "px";
-
-    div.appendChild(particles);
-
-    setTimeout(()=>{
-        particles.remove();
-
-    },2000);
-
-}
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove",(e)=>{
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+});
